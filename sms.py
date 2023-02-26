85ddf204c459b38c1b4edbe763a93c2714f4c23c
“phucnb” <kufuc2604@gmail.com>
2023-02-26 16:54:28 -0500

    add event to send sms and custom host

---
+++ sms.py
@@ -0,0 +1,25 @@
+import os, sys
+from twilio.rest import Client
+import datetime as dt
+
+TWILIO_SID = os.environ.get("TWILIO_SID")
+TWILIO_TOKEN = os.environ["TWILIO_TOKEN"]
+PHONE_NUMBER = os.environ["PHONE_NUMBER"]
+FROM_PHONE_NUMBER = os.environ["FROM_PHONE_NUMBER"]
+
+with open('log/connection1.log', 'r') as file:
+    log = file.readlines()
+
+if ('------' in log[-1]):
+    msg = ' '.join(log[-4:-2])
+else:
+    msg = log[-1]
+# print(msg)
+
+
+client = Client(TWILIO_SID, TWILIO_TOKEN)
+message = client.messages.create(
+body=msg,
+from_=FROM_PHONE_NUMBER,
+to=PHONE_NUMBER
+)
