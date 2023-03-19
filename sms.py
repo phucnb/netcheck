195b52423c4f9b2dc8ab1dfdf7b15feeb52562c5
“phucnb” <kufuc2604@gmail.com>
2023-03-19 18:07:35 -0400

    edit path and environment for service

--- sms.py
+++ sms.py
@@ -1,13 +1,12 @@
 import os, sys
 from twilio.rest import Client
 import datetime as dt
-
 TWILIO_SID = os.environ.get("TWILIO_SID")
-TWILIO_TOKEN = os.environ["TWILIO_TOKEN"]
-PHONE_NUMBER = os.environ["PHONE_NUMBER"]
-FROM_PHONE_NUMBER = os.environ["FROM_PHONE_NUMBER"]
+TWILIO_TOKEN = os.environ.get("TWILIO_TOKEN")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
+FROM_PHONE_NUMBER = os.environ.get("FROM_PHONE_NUMBER")
 
-with open('log/connection1.log', 'r') as file:
+with open('/home/netcheck/log/connection1.log', 'r') as file:
     log = file.readlines()
 
 if ('------' in log[-1]):
